f09fc2989608545a47833efcd7963c8e593d47f8
Aran Holbrook <91827424+holbs@users.noreply.github.com>
2023-10-21 12:52:06 +0100

    Add ClassicRightClick.ps1

---
+++ Remediations/ClassicRightClick.ps1
@@ -0,0 +1,30 @@
+#Region: Detect
+Try {
+    $RightClick = Get-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -ErrorAction Stop
+    If ($RightClick.'(default)' -eq "") {
+        Exit 0
+    } Else {
+        Exit 1
+    }
+} Catch {
+    Exit 1
+}
+#EndRegion
+#Region: Remediation
+Try {
+    $RightClick = Get-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -ErrorAction Stop
+    If ($RightClick.'(default)' -eq "") {
+        Exit 0
+    } Else {
+        Try {
+            New-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(default)" -Value "" -Force -ErrorAction Stop
+        } Catch {
+            Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(default)" -Value "" -Force
+        }
+    }
+} Catch {
+    New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(default)" -Value "" -Force
+    Stop-Process -ProcessName "Explorer"
+}
+#EndRegion
