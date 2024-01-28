e96f770cdfb10198112aacf89c4e73e9ef1588c8
holbs <aranholbrook@outlook.com>
2024-01-28 09:00:34 +0000

    Uploaded BlockAADWorkplaceJoin to present devices becoming 'Microsoft Entra registered'

---
+++ Remediations/BlockAADWorkplaceJoin.ps1
@@ -0,0 +1,16 @@
+#Region: Detection
+Try {
+    $BlockAADWorkplaceJoin = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" -ErrorAction Stop
+    If ($BlockAADWorkplaceJoin.BlockAADWorkplaceJoin -eq 1) {
+        Exit 0
+    } Else {
+        Exit 1
+    }
+} Catch {
+    Exit 1
+}
+#EndRegion
+#Region: Remediation
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin'
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin' -Name 'BlockAADWorkplaceJoin' -Value '1' -Type DWord -Force
+#EndRegion
