9647e03d327210c411dfbdb4a6c1e6eb57eeb0eb
holbs <91827424+holbs@users.noreply.github.com>
2024-11-17 19:01:33 +0000

    Tidied up Compliance

--- Compliance/DefenderAntivirus/DefenderAntivirusComplianceCheck.ps1
+++
@@ -1,9 +0,0 @@
-Try {
-    $WinDefend = Get-Service -Name WinDefend -ErrorAction Stop
-    If ($WinDefend.Status -eq "Running") {
-        $DefenderAntivirusComplianceCheck = $true
-    }
-} Catch {
-    $DefenderAntivirusComplianceCheck = $false
-}
-@{DefenderAntivirusComplianceCheck = $DefenderAntivirusComplianceCheck} | ConvertTo-Json -Compress
